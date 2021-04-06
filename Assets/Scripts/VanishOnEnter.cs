a921d3eb6ed7ff1b295abacd7760b3b7a394e34b
snailythefan <darkcheep@live.com.mx>
2021-04-05 21:41:57 -0500

    added final map
    also moved the sound triggers and added a ghost

---
+++ Assets/Scripts/VanishOnEnter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanishOnEnter : MonoBehaviour
+{
+
+	public GameObject ghost;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+    	if (other.tag == "Player")
+    	{
+    		ghost.SetActive(false);
+    	}
+    }
+}
